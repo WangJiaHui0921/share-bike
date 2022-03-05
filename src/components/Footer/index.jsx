866f605752a45b7aedf6b0841b613075a034226a
wangjiahui <wangjiahui@tom.com>
2022-03-05 10:40:49 +0800

    Footer开发完成

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import "./index.less"
 const Index = () => {
     return (
-        <div>
-            This is Footer
+        <div className="footer">
+            版权所有：朗科&爱豆(推荐使用谷歌浏览器,可以获得更佳操作页面体验) 技术支持：爱豆
         </div>
     );
 }

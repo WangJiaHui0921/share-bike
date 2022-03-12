ea359e34677ff32355e8bbd6dee1ccda4b0f9923
wangjiahui <wangjiahui@tom.com>
2022-03-12 09:03:27 +0800

    员工管理开发完成

---
+++ src/pages/BikeMap/index.jsx
@@ -0,0 +1,14 @@
+import { Card, Button, Select, DatePicker } from "antd";
+import "./../../style/card.less";
+
+const BikeMap = () => {
+    return (
+        <div className="wrap">
+            <Card className="wrap">
+                
+            </Card>
+        </div>
+    );
+}
+
+export default BikeMap;
